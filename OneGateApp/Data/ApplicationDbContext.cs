fd032b10d4dc4230d7412efebc6b5467188e0aa6
Erik Zhang <erik@neo.org>
2026-06-20 01:22:14 +0800

    Split cache data into a separate database

--- OneGateApp/Data/ApplicationDbContext.cs
+++ OneGateApp/Data/ApplicationDbContext.cs
@@ -6,33 +6,4 @@ public partial class ApplicationDbContext(DbContextOptions<ApplicationDbContext>
 {
     public DbSet<Setting> Settings { get; set; }
     public DbSet<Contact> Contacts { get; set; }
-    public DbSet<Banner> Banners { get; set; }
-    public DbSet<News> News { get; set; }
-    public DbSet<DApp> DApps { get; set; }
-
-    public void EnsureMigrations()
-    {
-        Migration_AddProperty_DApps_Version_20260611();
-        Migration_AddProperty_DApps_GameType_20260619();
-    }
-
-    void Migration_AddProperty_DApps_Version_20260611()
-    {
-        int dappVersionColumns = Database
-            .SqlQueryRaw<int>("SELECT COUNT(*) AS \"Value\" FROM pragma_table_info('DApps') WHERE name = 'Version'")
-            .AsEnumerable()
-            .Single();
-        if (dappVersionColumns == 0)
-            Database.ExecuteSqlRaw("ALTER TABLE [DApps] ADD COLUMN [Version] INTEGER NOT NULL DEFAULT 0");
-    }
-
-    void Migration_AddProperty_DApps_GameType_20260619()
-    {
-        int dappGameTypeColumns = Database
-            .SqlQueryRaw<int>("SELECT COUNT(*) AS \"Value\" FROM pragma_table_info('DApps') WHERE name = 'GameType'")
-            .AsEnumerable()
-            .Single();
-        if (dappGameTypeColumns == 0)
-            Database.ExecuteSqlRaw("ALTER TABLE [DApps] ADD COLUMN [GameType] TEXT NULL");
-    }
 }
