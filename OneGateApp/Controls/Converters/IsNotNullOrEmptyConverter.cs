feb9365c2101a751cf1d043661fb0397eaf93501
Jimmy <heiykjq54@hotmail.com>
2026-06-20 17:42:08 +0800

    feat(home): redesign home feed with card visuals

---
+++ OneGateApp/Controls/Converters/IsNotNullOrEmptyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace NeoOrder.OneGate.Controls.Converters;
+
+class IsNotNullOrEmptyConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            string text => !string.IsNullOrWhiteSpace(text),
+            null => false,
+            _ => true
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
