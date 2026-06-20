00e5ffdfdd9489b45eb0111ad60a55575f5ad57a
Jimmy <jimmy@r3e.network>
2026-06-20 15:36:44 +0800

    style: add visual system foundation

--- OneGateApp/Controls/Views/TabBarLabelColorConverter.cs
+++ OneGateApp/Controls/Views/TabBarLabelColorConverter.cs
@@ -19,3 +19,23 @@ class TabBarLabelColorConverter : IMultiValueConverter
         return null!;
     }
 }
+
+class TabBarBackgroundColorConverter : IMultiValueConverter
+{
+    static readonly Color Transparent = Colors.Transparent;
+
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length < 3) return Transparent;
+        if (values[0] is not string value) return Transparent;
+        if (values[1] is not string selectedTab) return Transparent;
+        if (values[2] is not Color selectedTabBackgroundColor) return Transparent;
+        if (value != selectedTab) return Transparent;
+        return selectedTabBackgroundColor;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        return null!;
+    }
+}
